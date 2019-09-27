e871a0c855ff4bda5782bfe8a6c4a981f8113f91
Alex Colombo <uricaine@hotmail.com>
2019-09-27 20:37:34 +0200

    new component

--- src/components/micomponente.js
+++ src/components/micomponente.js
@@ -1,3 +1,7 @@
 import React from 'react';
 
-class micomponente extends React.Component {}
+class micomponente extends React.Component {
+  render() {
+    return <h1>Hola Alex desde new component</h1>;
+  }
+}

679c117d15a4a4570081bec39c7a5739c56b3fab
Alex Colombo <uricaine@hotmail.com>
2019-09-29 22:57:06 +0200

    refactor

--- src/components/micomponente.js
+++ src/components/micomponente.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 
 class MiComponente extends Component {
   render() {
-    return <h1>Hola Alex desde new component</h1>;
+    return (
+    <h1>Hola Alex desde new component</h1>;
+    )
   }
 }
 

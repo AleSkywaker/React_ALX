42e6bb099650c3f434c45a1b4919b0975585fc1c
Alex Colombo <uricaine@hotmail.com>
2019-10-01 23:05:28 +0200

    creating header

--- src/App.js
+++ src/App.js
@@ -2,40 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './assets/css/App.css';
 
-import MiComponente from './components/MiComponente';
+import Header from './components/Header';
 
 function App() {
-  let name = 'Alex';
-
-  function holaMundo(nombre) {
-    let presentacion = (
-      <div>
-        <h2>Esto es una prueba</h2>
-        <h3> Hola {nombre} ¿Que tal?</h3>
-      </div>
-    );
-
-    return presentacion;
-  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {console.log('hola')}
-        {holaMundo(name)}
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprender React
-        </a> */}
-        <MiComponente />
-      </header>
+      <Header />
     </div>
   );
 }

98b81430f3191158a0d82c49d2c2308c80fb4643
Alex Colombo <uricaine@hotmail.com>
2019-10-01 23:10:52 +0200

    creating slider component

--- src/components/slider.js
+++ src/components/slider.js
@@ -3,7 +3,10 @@ import React, {Component} from React;
 class Slider extends Component {
 
   render(){
-
+  <div id="slider" className="slider-big">
+    <h1>Bienvenido Alex</h1>
+    <a href="#" className="btn-white">Ir al blog</a>
+  </div>
   }
 
 }

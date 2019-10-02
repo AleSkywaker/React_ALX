3a36fa5bd8c68f131b5664750cfe5ca3eb7fada9
Alex Colombo <uricaine@hotmail.com>
2019-10-02 20:45:53 +0200

    new component sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,8 @@
-import React, {Component} from 'react';
-
+import React, { Component } from 'react';
 
 class Sidebar extends Component {
-
-render(){
-  return (
-    <h1>Sidebar</h1>
-  )
+  render() {
+    return <h1>Sidebar</h1>;
+  }
 }
-
-}
+export default Sidebar;

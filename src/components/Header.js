f78e2fe53e6a0a2c8f90bc15da37704af7254538
Alex Colombo <uricaine@hotmail.com>
2019-10-03 19:28:58 +0200

    new component sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,41 +4,41 @@ import logo from '../logo.svg';
 class Header extends Component {
   render() {
     return (
-        <header id="header">
-          <div className="center">
-            {/* LOGO */}
-            <div id="logo">
-              <img src={logo} className="app-logo" alt="Logotipo" />
-              <span id="brand">
-                <strong>Curso</strong>React
-              </span>
-            </div>
-
-            {/* MENU */}
-            <nav id="menu">
-              <ul>
-                <li>
-                  <a href="index.html">Inicio</a>
-                </li>
-                <li>
-                  <a href="blog.html">Blog</a>
-                </li>
-                <li>
-                  <a href="formulario.html">Formulario</a>
-                </li>
-                <li>
-                  <a href="#">Pagina 1</a>
-                </li>
-                <li>
-                  <a href="#">Pagina 2</a>
-                </li>
-              </ul>
-            </nav>
-
-            {/* LIMPIAR FLOTADOS */}
-            <div className="clearfix"></div>
+      <header id="header">
+        <div className="center">
+          {/* LOGO */}
+          <div id="logo">
+            <img src={logo} className="app-logo" alt="Logotipo" />
+            <span id="brand">
+              <strong>React</strong>for Alex
+            </span>
           </div>
-        </header>
+
+          {/* MENU */}
+          <nav id="menu">
+            <ul>
+              <li>
+                <a href="index.html">Inicio</a>
+              </li>
+              <li>
+                <a href="blog.html">Blog</a>
+              </li>
+              <li>
+                <a href="formulario.html">Formulario</a>
+              </li>
+              <li>
+                <a href="#">Pagina 1</a>
+              </li>
+              <li>
+                <a href="#">Pagina 2</a>
+              </li>
+            </ul>
+          </nav>
+
+          {/* LIMPIAR FLOTADOS */}
+          <div className="clearfix"></div>
+        </div>
+      </header>
     );
   }
 }

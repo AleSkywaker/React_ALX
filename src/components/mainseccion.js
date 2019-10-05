75df4de515da4be8a1242f7389fc814aed10f719
Alex Colombo <uricaine@hotmail.com>
2019-10-05 12:34:03 +0200

    new component main seccion

--- src/components/mainseccion.js
+++ src/components/mainseccion.js
@@ -1,8 +1,45 @@
 import React, { Component } from 'react';
 
 class Seccion extends Component {
+  /*
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contador: 0
+    };
+  }
+ */
+
+  state = {
+    contador: 0
+  };
+  sumar = e => {
+    console.log(this);
+    console.log(e);
+    this.setState({
+      contador: this.state.contador + 1
+    });
+  };
+  restar = () => {
+    this.setState({
+      contador: this.state.contador - 1
+    });
+  };
   render() {
-    return <h1>Pruebas</h1>;
+    return (
+      <section id="content">
+        <h2 className="subheader">Últimos articulos</h2>
+        <section className="componentes">
+          <h4>Estado</h4>
+          <p>Contador : {this.state.contador}</p>
+          <form>
+            <input type="button" value="Sumar" onClick={this.sumar} />
+            <input type="button" value="Restar" onClick={this.restar} />
+          </form>
+        </section>
+      </section>
+    );
   }
 }
 

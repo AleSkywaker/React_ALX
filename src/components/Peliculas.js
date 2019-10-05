59750b1415086592c42acdf726a8f8512ceee47d
Alex Colombo <uricaine@hotmail.com>
2019-10-05 13:22:06 +0200

    new component

---
+++ src/components/Peliculas.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Peliculas extends Component {
+  render() {
+    return (
+      <div id="peliculas">
+        <h4>Peliculas</h4>
+      </div>
+    );
+  }
+}

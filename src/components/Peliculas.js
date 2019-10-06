d6bcf4047820dccbc8e6b30c26e12732db8b9411
Alex Colombo <uricaine@hotmail.com>
2019-10-06 12:42:21 +0200

    import peliculas component

--- src/components/Peliculas.js
+++ src/components/Peliculas.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import Pelicula from './Pelicula';
 
 class Peliculas extends Component {
+  state = {
+    peliculas: [
+      {
+        titulo: 'Blade Runner',
+        imagen:
+          'https://m.guiadelocio.com/var/guiadelocio.com/storage/images/cine/archivo-peliculas/blade-runner-2049/32017954-10-esl-ES/blade-runner-2049.jpg'
+      },
+      {
+        titulo: 'Septimo Sello',
+        imagen: 'https://democresia.es/wp-content/uploads/2018/02/TheSeventhSeal1_source.jpg'
+      },
+      {
+        titulo: 'Pulp Fiction',
+        imagen: 'https://pics.filmaffinity.com/Pulp_Fiction-210382116-large.jpg'
+      }
+    ],
+    nombre: 'Alex Colombo'
+  };
   render() {
     return (
-      <div id="peliculas">
-        <h4>Peliculas</h4>
+      <div id="content" className="peliculas">
+        <h2 className="subheader">Peliculas</h2>
+        <p>Peliculas favoritas de {this.state.nombre}</p>
+
+        {/**Crear componentes peliculas */}
+        <div id="articles" className="peliculas">
+          {this.state.peliculas.map((peli, i) => {
+            return <Pelicula key={i} pelicula={peli} />;
+          })}
+        </div>
       </div>
     );
   }
 }
+export default Peliculas;

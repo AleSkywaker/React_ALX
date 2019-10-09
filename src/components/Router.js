299f0287365c5f462f6ff4d9ff8f850656f753e1
Alex Colombo <uricaine@hotmail.com>
2019-10-09 20:45:31 +0200

    new router component

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 
 class Router extends React.Component {
   render() {
+    {
+      /*Configurar rutas y páginas*/
+    }
     return <h1>Router</h1>;
   }
 }

4bbf19b4146ab20a97223a8f364eec83bb900d11
Alex Colombo <uricaine@hotmail.com>
2019-10-10 20:05:03 +0200

    router component

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Seccion from './Mainseccion';
+import Peliculas from './Peliculas';
+import Error from './Error';
 
 class Router extends React.Component {
   render() {
     return (
       <BrowserRouter>
         {/*Configurar rutas y páginas*/}
-        <Switch></Switch>
+        <Switch>
+          <Route exact path="/" component={Peliculas} />
+          <Route exact path="/prueba" component={Seccion} />
+          <Route component={Error} />
+        </Switch>
       </BrowserRouter>
     );
   }

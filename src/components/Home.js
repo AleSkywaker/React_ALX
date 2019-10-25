712535a547e1a6703c0ac39cd7af3471ea38d4b7
Alex Colombo <uricaine@hotmail.com>
2019-10-25 21:40:42 +0200

    restructuring

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import Slider from './Slider';
 
 class Home extends Component {
-  render() {pp
+  render() {
     return (
       <div id="content">
         <Slider titulo="Bienvenidos al Blog de Alex" btn="Ir al blog" />
         <div className="center">
           <h1 className="subheader">últimos Articulos</h1>
         </div>
+        <Sidebar />
       </div>
     );
   }
